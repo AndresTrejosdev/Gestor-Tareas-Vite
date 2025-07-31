b57f0f1197b53c9de5a35c213bf78291d2e08364
AndresTrejosdev <andres.trejos.dev@gmail.com>
2025-07-31 14:41:34 -0500

    act

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="aplicacion-tareas">
       <div className="tareas-lista-principal">
-        <h1>Mis Tareas</h1>
+        <h1>Mis Tareas  nuevas</h1>
         <ListaDeTareas />
       </div>
     </div>
